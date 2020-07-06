3524cb91fe6218297bef11e9023684f4bfc0bf32
Luiz Mendes <developer.luizmendes@gmail.com>
2020-07-05 22:06:49 -0300

    teste heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+const PORT = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/dist/ffa-final'));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+    res.sendFile(__dirname+"/dist/ffa-final/index.html");
 });
  
-app.listen(process.env.PORT || 8080);
+app.listen(PORT,()=>{
+    console.log("iniciado")
+});
